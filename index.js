01285c17aa013c13b1f59b3b133c0c1e46ddf253
blessedsoy <unmigong@gmail.com>
2016-11-03 16:35:52 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+const sortedRobots = robots.map(function(robot){
+  const isDecepticon = knownDecepticons.includes(robot.name);
+  return Object.assign({}, robot, {
+      if (isDecepticon){
+        alliance: 'deceptiocn'
+      } else {
+        alliance: 'autobot'
+      }
+  });
+});
+
+
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +34,10 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+
+const coloredZebraStripes = zebraStripes.map((stripe, index) => {
+  return Object.assign({}, stripe, {
+      color: index % 2 == 0 ? 'black' : 'white'
+  })
+})
