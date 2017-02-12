78f6e6e2cd998ac6e29cb7d37bc32a1b60b728d9
paige11 <ellis.paige.v@gmail.com>
2017-02-11 22:12:49 -0500

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,27 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+const sortedRobots = robots.map(function(bot) {
+  if (knownDecepticons.includes(bot.name)) {
+    return Object.assign({}, bot, {
+      alliance: 'decepticon'
+    });
+  } else {
+    return Object.assign({}, bot, {
+      alliance: 'autobot'
+    });
+  }
+});
+
+const coloredZebraStripes = zebraStripes.map(function(stripe, i) {
+  if (i % 2 === 0) {
+    return Object.assign({}, stripe, {
+      color: 'black'
+    });
+  } else {
+    return Object.assign({}, stripe, {
+      color: 'white'
+    });
+  }
+})
